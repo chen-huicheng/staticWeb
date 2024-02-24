90259c824a6f4f2ee6709100bd8cd9852b42745e
huicheng_chen <huicheng_chen@qq.com>
2024-02-25 04:03:19 +0800

    240225

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/chen-huicheng/staticWeb.git master
+git push -f https://github.com/chen-huicheng/leoblog.git master
 
 cd -
